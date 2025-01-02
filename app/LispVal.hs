972793172d159648ea3f2f9f1e64678118ceb9b7
Shiva Tamil Kumaran <shivatk01@gmail.com>
2025-01-02 22:44:47 +0000

    Created LispVal.

---
+++ app/LispVal.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module LispVal where
+
+import qualified Data.Map as Map
+import qualified Data.Text as T
+
+import Control.Monad.Except
+import Control.Monad.Reader
+
+type Env = Map.Map T.Text LispVal
+
+type Eval a = ReaderT Env IO a
+
+type Func = [LispVal] -> Eval LispVal
+
+data LispVal = Atom T.Text
+             | List [LispVal]
+             | Number Integer
+             | String T.Text
+             | Func Func
+             | Lambda Func Env
+             | Nil
+             | Bool Bool
+             deriving (Eq)
+
+instance Show LispVal where
+  show = T.unpack . showVal
+    where
+      showVal :: LispVal -> T.Text
+      showVal (Atom x)     = x
+      showVal (String s)   = s
+      showVal (Number n)   = T.pack (show n)
+      showVal (Bool True)  = "#t"
+      showVal (Bool False) = "#f"
+      showVal Nil          = "Nil"
+      showVal (List vs)    = T.concat [ "(", T.unwords (map showVal vs), ")" ]
+      showVal (Fun _)      = "(internal function)"
+      showVal (Lambda _ _) = "(lambda function)"
